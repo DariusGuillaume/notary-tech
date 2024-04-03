860b478d49ca875523a7487b566571b93eded562
DariusGuillaume <dariusguillaume2@gmail.com>
2024-04-03 12:45:35 -0400

    added theme provider

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <div>Homepage</div>
+      {/* Add your homepage content here */}
+    </main>
+  );
+}

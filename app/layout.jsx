867da3eb40bddc3016035a78f6f9ced3d0eb3f35
DariusGuillaume <dariusguillaume2@gmail.com>
2024-04-05 17:36:58 -0400

    changed fonts

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,15 @@
-import { Inter } from "next/font/google";
+import { Inter, Josefin_Sans } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import ThemeProvider from "@/components/ThemeProvider";
 
 const inter = Inter({ subsets: ["latin"] });
+const josefinSans = Josefin_Sans({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700"],
+  display: "swap",
+});
 
 export const metadata = {
   title: "Notary Site",
@@ -14,15 +19,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      
-      <body className={inter.className}>
-        <ThemeProvider attribute = "class" defaultTheme= "light" >
-      <Header/>
-        {children}
-        <Footer />
+      <body className={`${inter.className} ${josefinSans.className}`}>
+        <ThemeProvider attribute="class" defaultTheme="light">
+          <Header />
+          {children}
+          <Footer />
         </ThemeProvider>
-        </body>
-     
+      </body>
     </html>
   );
 }

7b34167a2e0d0428afa15f6b140b710925873e5f
DariusGuillaume <dariusguillaume2@gmail.com>
2024-04-07 08:05:06 -0400

    added hero section

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -4,6 +4,7 @@ import {Send } from "lucide-react"
 
 
 
+
 import React from 'react'
 
 import { RiTeamFill, RiTodoFill, RiArrowDownSLine } from "react-icons/ri";
@@ -17,20 +18,29 @@ const Hero = () => {
         <div className='container mx-auto'>
         <div className='flex justify-between gap-x-8'> 
         {/* text /> */}
-        <div> 
-        <div> Notary Services</div>
-        <h1> We are here to help you</h1>
-        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in quam id purus mollis aliquam. </p>
+        <div className='flex max-w-[600px] flex-col justify-center mx-auto xl:mx-0 text-center xl:text-left'> 
+        <div className='text-sm uppercase font-semibold mb-4 text-primary tracking-[4px]'> Notary Services</div>
+        <h1 className='h1'> We are here to help you</h1>
+        <p className='subtitle max-w-[490px] mx-auto xl:mx-0'> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in quam id purus mollis aliquam. </p>
 
+         {/* buttons */}
+         <div className='flex flex-col gap-y-3 md:flex-row gap-x-3 mx-auto xl:mx-0 mb-12'> 
+         <Link href="/contact">
+  <Button className="gap-x-2">
+    Contact Us <Send size={18} />
+  </Button>
+</Link>
+        
+         </div>
         </div>
         {/* <HomeImage /> */}
-        <div> Home Image</div>
+        <div className='hidden xl:flex relative'> Home Image</div>
 
         </div>
         {/*  icon*/}   
-        <div className='hidden md:flex absolute left-2/4 bottom-44 xl:bottom-12 animate-bounce  '> 
-        <RiArrowDownSLine className='text-3xl text-primary text-gray-500' />
-        </div>
+      <div className='hidden xl:flex absolute left-2/4 bottom-44 xl:bottom-12 animate-bounce'>
+  <RiArrowDownSLine className='text-3xl text-primary text-gray-500' />
+</div>
         </div>
     </section> )
 };
